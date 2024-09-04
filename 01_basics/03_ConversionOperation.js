25b638f71328dad234b83ff5a8e944546d5908b4
Sweety <138968259+GitSweety-21@users.noreply.github.com>
2024-09-04 16:16:31 +0000

    today commit

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -25,5 +25,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let sumNumber =33;
 let stringNumber = String(sumNumber);
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+//console.log(stringNumber)
+//console.log(typeof(stringNumber))
+
+
+// *********************** Operations *******************//
+
+let value = 3;
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+// let str1 = "sweety"
+// let str2 = " Behera"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1,num2,num3
+
+num1 = num2 = num3 =2+2
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
