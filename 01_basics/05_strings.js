8926ab3ec6c5a01d33df17a592e7dc2d6224bc48
Sweety <138968259+GitSweety-21@users.noreply.github.com>
2024-09-05 16:53:59 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,43 @@
+const name = "Sweety"
+const repoCount = 50
+
+//console.log(name + repoCount + " value");
+ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+ const gameName = new String('sweety-om-com')
+
+
+ console.log(gameName[0]);
+ console.log(gameName.__proto__);
+
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('t'));
+
+ const newString = gameName.substring(0,4)
+ console.log(newString);
+
+ const anotherString = gameName.slice(-8,4)
+ console.log(anotherString);
+
+ const newString1 = "     sweety    "
+ console.log(newString1);
+ console.log(newString1.trim());
+
+
+ const url = "https://sweetyy.com/sweety%20behera"
+
+console.log (url.replace('%20','-'))
+
+console.log(url.includes('sweety'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'))
+
+
+
+
+
